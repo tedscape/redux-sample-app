1e78a6b125c89e3061f4cfd8f1c544cab23472d4
Jacob Sebastian <jacobsebastian@outlook.com>
2016-04-01 09:39:42 +1100

    finng tests. Loading entries

--- src/core.js
+++ src/core.js
@@ -1,4 +1,6 @@
+import {List} from 'immutable';
+
 export function setEntries(state, entries){
     "use strict";
-    return state.set('entries',entries);
+    return state.set('entries',List(entries));
 }
--- test/core_spec.js
+++ test/core_spec.js
@@ -16,11 +16,11 @@ describe('application logic', ()=>{
        });
         it('converts to immutable',()=>{
 
-            const state=Map();
-            const entries=['Trainspotting','28 days later'];
-            const nextState= setEntries(state,entries);
+            const state = Map();
+            const entries = ['Trainspotting', '28 Days Later'];
+            const nextState = setEntries(state, entries);
             expect(nextState).to.equal(Map({
-                entries:List.of( 'Trainspotting','28 days later')
+                entries: List.of('Trainspotting', '28 Days Later')
             }));
 
         });
